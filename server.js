bfa7ae7f7f21c9a039da2d918d588845bb2deafe
ericjosprice <epriceutexas@gmail.com>
2019-04-06 17:08:07 -0500

    removed node_modules

--- server.js
+++ server.js
@@ -3,3 +3,25 @@ var app = express();
 var path = require("path");
 
 
+var PORT =  process.env.PORT || 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//get home.html when the domain is called
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+
+});
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
